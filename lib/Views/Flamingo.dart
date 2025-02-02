4f7ce3043e86d65f10c64edb11ba0b9901140e25
guvencburak10 <guvencburak10@gmail.com>
2025-02-03 01:00:48 +0300

    Schedule Sayfası oluşturuldu ve Flamingo sayfası için dialog çalışması yapıldı.

--- lib/Views/Flamingo.dart
+++ lib/Views/Flamingo.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class Flamingo extends StatefulWidget {
   @override
@@ -10,11 +11,35 @@ class _Flamingo extends State<Flamingo> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Text("Flamingo")
-        ],
+      child: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                child: Text('Basic Alert'),
+                onPressed: () => showAlert(context),
+              ),
+            ],
+        ),
       ),
     );
   }
+}
+
+@override
+void initState() {
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+    //showAlert(context);
+  });
+}
+
+void showAlert(BuildContext context){
+  Alert(
+    context: context,
+    title: "Neden Flamingo?",
+    style: AlertStyle(titleStyle: TextStyle(fontSize: 25,color: Colors.black,fontFamily:'Arimo-Bold',)),
+    desc: "Flamingo denge, duyarlılık, yeniden doğuş, fedakarlık, çekicilik ve zarafetin sembolüdür ve dört elementle ilişkilendirilir:\n"
+        " Su, hava, ateş ve toprak. Kendini beslemek için daldığı su, ruhu, insan ruhunu ve arınmayı temsil eder. Hava, maneviyatımızdaki görünmezin, uçuşun sembolüdür",
+    image: Image.asset("assets/flamingo.png",width: 100,height: 100,)
+  ).show();
 }
--- lib/Views/Schedule.dart
+++ lib/Views/Schedule.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Schedule extends StatefulWidget {
   @override
@@ -7,14 +7,49 @@ class Schedule extends StatefulWidget {
 }
 
 class _Schedule extends State<Schedule> {
+  late WebViewController _controller;
+  var loadingPercentage = 0;
+  String URL = "https://linktr.ee/flamingoshouse?fbclid=PAZXh0bgNhZW0CMTEAAaZJXMHdDz_cgBKgAFDXaR4PNUAQ24IYZINFHHzZSNCitZe-Dps2aO6wtyM_aem_arNrbdGuCYZCAw7da4C1Ow";
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text("Schedule")
-        ],
-      ),
+    return Stack(
+      children: [
+        WebViewWidget(controller: _controller,
+        ),
+        loadingPercentage < 100
+        ?LinearProgressIndicator(
+          color: Colors.pinkAccent,
+          value: loadingPercentage / 100,
+        )
+            :Container()
+      ],
     );
   }
+
+  @override
+  void initState(){
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(NavigationDelegate(
+      onPageStarted: (url) {
+        setState(() {
+          loadingPercentage = 0;
+        });
+      },
+      onProgress: (progress){
+        setState(() {
+          loadingPercentage = progress;
+        });
+      },
+      onPageFinished: (url) {
+        setState(() {
+          loadingPercentage = 100;
+        });
+      }
+    ))
+    ..loadRequest(Uri.parse(URL));
+  }
+
 }
+
